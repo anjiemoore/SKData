3d2ad1ab1c830dcf0f68b23c672b3ad89968992b
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-08-24 01:47:45 -0600

    trying to figure out how to scrape without classes in wiki

--- scrape.py
+++ scrape.py
@@ -4,6 +4,12 @@ from bs4 import BeautifulSoup
 url = 'https://en.wikipedia.org/wiki/List_of_serial_killers_in_the_United_States'
 
 req = requests.get(url)
-
 soup = BeautifulSoup(req.text, 'html.parser')
-print(soup)
+
+sk_table = soup.find('table', class_ = 'wikitable')
+
+for skill in sk_table.find_all('tbody'):
+    rows = skill.find_all('tr')
+    for row in rows:
+        names = row.find('td')
+        print(names)
